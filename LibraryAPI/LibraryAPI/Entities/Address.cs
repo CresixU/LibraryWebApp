b644b287870a75f8a5709d30b311dafb19647547
Krystian Wawruch <CresixU@gmail.com>
2023-03-13 22:12:22 +0100

    Added PostalCode property

--- LibraryAPI/LibraryAPI/Entities/Address.cs
+++ LibraryAPI/LibraryAPI/Entities/Address.cs
@@ -3,6 +3,7 @@
     public class Address
     {
         public int Id { get; set; }
+        public string PostalCode { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
         public string Number { get; set; }
