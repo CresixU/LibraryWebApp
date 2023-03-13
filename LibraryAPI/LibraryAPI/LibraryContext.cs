3fe485a1711bd8fba478bcdf32b230cf4e122a02
Krystian Wawruch <CresixU@gmail.com>
2023-03-13 23:42:37 +0100

    Added Requirements and References to tables

--- LibraryAPI/LibraryAPI/LibraryContext.cs
+++ LibraryAPI/LibraryAPI/LibraryContext.cs
@@ -17,7 +17,60 @@ namespace LibraryAPI
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<Address>(eb =>
+            {
+                eb.Property(a => a.City).HasMaxLength(50).IsRequired();
+                eb.Property(a => a.Street).HasMaxLength(50).IsRequired();
+                eb.Property(a => a.Number).HasMaxLength(10).IsRequired();
+                eb.Property(a => a.PostalCode).HasMaxLength(10).IsRequired();
+            });
+
+
+            modelBuilder.Entity<Book>(eb =>
+            {
+                eb.Property(b => b.Author).HasMaxLength(50).IsRequired();
+                eb.Property(b => b.Title).HasMaxLength(100).IsRequired();
+                eb.Property(b => b.PublicationYear).HasMaxLength(4);
+
+                eb.HasMany(b => b.Rents)
+                    .WithMany(re => re.Books);
+                eb.HasOne(b => b.Category)
+                    .WithMany(c => c.Books)
+                    .HasForeignKey(b => b.CategoryId);
+            });
+
+            modelBuilder.Entity<Category>(eb =>
+            {
+                eb.Property(c => c.Name).HasMaxLength(50).IsRequired();
+            });
+
+            modelBuilder.Entity<Rent>(eb =>
+            {
+                eb.Property(re => re.RentDate).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<Role>(eb =>
+            {
+                eb.Property(ro => ro.Name).HasMaxLength(50).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(eb =>
+            {
+                eb.Property(u => u.Firstname).HasMaxLength(50).IsRequired();
+                eb.Property(u => u.Lastname).HasMaxLength(50).IsRequired();
+                eb.Property(u => u.Email).HasMaxLength(50).IsRequired();
+                eb.Property(u => u.Password).IsRequired();
+
+                eb.HasOne(u => u.Address)
+                    .WithMany(a => a.Users)
+                    .HasForeignKey(u => u.AddressId);
+                eb.HasOne(u => u.Role)
+                    .WithMany(ro => ro.Users)
+                    .HasForeignKey(u => u.RoleId);
+                eb.HasMany(u => u.Rents)
+                    .WithOne(re => re.User)
+                    .HasForeignKey(re => re.UserId);
+            });
         }
 
 
