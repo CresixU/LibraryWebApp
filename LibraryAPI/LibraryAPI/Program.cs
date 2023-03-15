6f2a928fe0bb9d68bf09f45ce51d74f5d5382f16
Krystian Wawruch <CresixU@gmail.com>
2023-03-15 12:29:21 +0100

    Added Seeder and fix infinite loop

--- LibraryAPI/LibraryAPI/Program.cs
+++ LibraryAPI/LibraryAPI/Program.cs
@@ -1,5 +1,6 @@
 using LibraryAPI;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<LibrarySeeder>();
 
 builder.Services.AddDbContext<LibraryContext>(
     option => option.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnectionString"))
     );
+builder.Services.AddControllers().AddJsonOptions(
+    option => option.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve
+    );
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<LibrarySeeder>();
+seeder.Seed();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
