93510f98bd25759c030e0ae56cbc941b549e9931
Krystian Wawruch <CresixU@gmail.com>
2023-03-16 13:24:02 +0100

    Added Mapping for user create and update

--- LibraryAPI/LibraryAPI/LibraryMappingProfile.cs
+++ LibraryAPI/LibraryAPI/LibraryMappingProfile.cs
@@ -14,6 +14,24 @@ namespace LibraryAPI
                 .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
                 .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode));
 
+            CreateMap<User, UserCreateDTO>()
+                .ForMember(a => a.City, b => b.MapFrom(c => c.Address.City))
+                .ForMember(a => a.Street, b => b.MapFrom(c => c.Address.Street))
+                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
+                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode));
+
+            CreateMap<User, UserUpdateDTO>()
+                .ForMember(a => a.City, b => b.MapFrom(c => c.Address.City))
+                .ForMember(a => a.Street, b => b.MapFrom(c => c.Address.Street))
+                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
+                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode));
+
+            CreateMap<User, UserDTO>()
+                .ForMember(a => a.City, b => b.MapFrom(c => c.Address.City))
+                .ForMember(a => a.Street, b => b.MapFrom(c => c.Address.Street))
+                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
+                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode))
+                .ForMember(a => a.Role, b => b.MapFrom(c => c.Role.Name));
         }
     }
 }
