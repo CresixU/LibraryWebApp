1149a26a5cc0c211e0303ff0fc5ba9bb16d03b6e
Krystian Wawruch <CresixU@gmail.com>
2023-03-16 09:45:09 +0100

    Create UsersDTO.cs

---
+++ LibraryAPI/LibraryAPI/Models/UsersDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryAPI.Models
+{
+    public class UsersDTO
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
