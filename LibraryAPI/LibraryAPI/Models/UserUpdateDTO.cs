51d1e9aee39e74cfa6676ecca641e8f5621f9bd6
Krystian Wawruch <CresixU@gmail.com>
2023-03-26 23:48:59 +0200

    Removed required attribute

--- LibraryAPI/LibraryAPI/Models/UserUpdateDTO.cs
+++ LibraryAPI/LibraryAPI/Models/UserUpdateDTO.cs
@@ -6,17 +6,13 @@ namespace LibraryAPI.Models
     public class UserUpdateDTO
     {
         public int Id { get; set; }
-        [Required]
         [MaxLength(50)]
         public string Firstname { get; set; }
-        [Required]
         [MaxLength(50)]
         public string Lastname { get; set; }
-        [Required]
         [MaxLength(50)]
         [EmailAddress]
         public string Email { get; set; }
-        [Required]
         public string Password { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
