dcf67cdc71a2e6987399e8bd2ad8a9d6bd30b254
Krystian Wawruch <CresixU@gmail.com>
2023-03-26 23:48:28 +0200

    Corrected mapping

--- LibraryAPI/LibraryAPI/LibraryMappingProfile.cs
+++ LibraryAPI/LibraryAPI/LibraryMappingProfile.cs
@@ -12,25 +12,31 @@ namespace LibraryAPI
                 .ForMember(a => a.City, b => b.MapFrom(c => c.Address.City))
                 .ForMember(a => a.Street, b => b.MapFrom(c => c.Address.Street))
                 .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
-                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode));
+                .ForMember(a => a.PostalCode, b => b.MapFrom(c => c.Address.PostalCode));
+            
+            CreateMap<UserCreateDTO, User>()
+                .ForMember(a => a.Address, b => b.MapFrom(dto => new Address()
+                {
+                    City = dto.City,
+                    Street = dto.Street,
+                    Number = dto.Number,
+                    PostalCode = dto.PostalCode
+                }));
 
-            CreateMap<User, UserCreateDTO>()
-                .ForMember(a => a.City, b => b.MapFrom(c => c.Address.City))
-                .ForMember(a => a.Street, b => b.MapFrom(c => c.Address.Street))
-                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
-                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode));
-
-            CreateMap<User, UserUpdateDTO>()
-                .ForMember(a => a.City, b => b.MapFrom(c => c.Address.City))
-                .ForMember(a => a.Street, b => b.MapFrom(c => c.Address.Street))
-                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
-                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode));
+            CreateMap<UserUpdateDTO, User>()
+                .ForMember(a => a.Address, b => b.MapFrom(dto => new Address()
+                {
+                    City = dto.City,
+                    Street = dto.Street,
+                    Number = dto.Number,
+                    PostalCode = dto.PostalCode
+                }));
 
             CreateMap<User, UserDTO>()
                 .ForMember(a => a.City, b => b.MapFrom(c => c.Address.City))
                 .ForMember(a => a.Street, b => b.MapFrom(c => c.Address.Street))
                 .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.Number))
-                .ForMember(a => a.Number, b => b.MapFrom(c => c.Address.PostalCode))
+                .ForMember(a => a.PostalCode, b => b.MapFrom(c => c.Address.PostalCode))
                 .ForMember(a => a.Role, b => b.MapFrom(c => c.Role.Name));
         }
     }
