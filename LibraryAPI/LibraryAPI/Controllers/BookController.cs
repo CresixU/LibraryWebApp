47b172ee41fe12cf39e4c5d29f13ba2052efcf7d
Krystian Wawruch <CresixU@gmail.com>
2023-04-01 13:20:58 +0200

    Add Post, Put and Delete + Refactor for services

--- LibraryAPI/LibraryAPI/Controllers/BookController.cs
+++ LibraryAPI/LibraryAPI/Controllers/BookController.cs
@@ -1,4 +1,6 @@
 ﻿using LibraryAPI.Entities;
+using LibraryAPI.Models.Book;
+using LibraryAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryAPI.Controllers
@@ -6,31 +8,56 @@ namespace LibraryAPI.Controllers
     [Route("api/books")]
     public class BookController : ControllerBase
     {
-        private readonly LibraryContext _dbContext;
+        private readonly IBookService _service;
 
-        public BookController(LibraryContext dbContext)
+        public BookController(IBookService service)
         {
-            _dbContext = dbContext;
+            _service = service;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Book>> GetAll()
+        public ActionResult<IEnumerable<BookDTO>> GetAll()
         {
-            var books = _dbContext.Books.ToList();
-
+            var books = _service.GetAll();
             return Ok(books);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<IEnumerable<Book>> Get([FromRoute] int id)
+        public ActionResult<IEnumerable<BookDTO>> Get([FromRoute] int id)
         {
-            var book = _dbContext
-                .Books
-                .FirstOrDefault(x => x.Id == id);
+            var book = _service.Get(id);
 
-            if (book is null) return NotFound();
+            if(book is null) return NotFound();
 
             return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] BookCreateDTO dto)
+        {
+            var bookId = _service.Create(dto);
+
+            return Created($"api/books/{bookId}", null);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update([FromRoute] int id, [FromBody] BookUpdateDTO dto)
+        {
+            var isUpdated = _service.Update(id, dto);
+            
+            if(!isUpdated) return NotFound();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            var isDeleted = _service.Delete(id);
+
+            if(!isDeleted) return NotFound();
+
+            return NoContent();
         }
     }
 }
