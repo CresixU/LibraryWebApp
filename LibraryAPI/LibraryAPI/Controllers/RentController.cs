919e467e1f0aa14e2506b65962578689cdbfd860
Krystian Wawruch <CresixU@gmail.com>
2023-04-03 21:59:42 +0200

    Refactor and add Post and Put for rents

--- LibraryAPI/LibraryAPI/Controllers/RentController.cs
+++ LibraryAPI/LibraryAPI/Controllers/RentController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryAPI.Entities;
+using LibraryAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryAPI.Controllers
@@ -6,31 +7,46 @@ namespace LibraryAPI.Controllers
     [Route("api/rents")]
     public class RentController : ControllerBase
     {
-        private readonly LibraryContext _dbContext;
+        private readonly IRentService _service;
 
-        public RentController(LibraryContext dbContext)
+        public RentController(IRentService service)
         {
-            _dbContext = dbContext;
+            _service = service;
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Rent>> GetAll()
         {
-            var rents = _dbContext.Rents.ToList();
+            var rents = _service.GetAll();
 
             return Ok(rents);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Rent> Get([FromRoute] int id)
+        public ActionResult<Rent> GetAllByUserId([FromRoute] int id)
         {
-            var rent = _dbContext
-                .Rents
-                .FirstOrDefault(r => r.Id == id);
+            var rent = _service.GetAllByUserId(id);
 
             if (rent is null) return NotFound();
 
             return Ok(rent);
+        }
+
+        [HttpPost]
+        public ActionResult RentBooks(List<int> bookIds)
+        {
+            var id = _service.RentBooks(bookIds);
+            if (id == 0) return NoContent();
+            return Created($"api/rents/{id}", null);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult ReturnBooks(int rentId, List<int> bookIds)
+        {
+            var isUpdated = _service.ReturnBooks(rentId, bookIds);
+            if(!isUpdated) return NotFound();
+
+            return Ok();
         }
     }
 }
