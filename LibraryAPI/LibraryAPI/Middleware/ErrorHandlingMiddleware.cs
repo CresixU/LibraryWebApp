5afc5c329927a7f4fe400d6941a45fb04f47d3c2
Krystian Wawruch <CresixU@gmail.com>
2023-04-16 11:01:17 +0200

    Create ErrorHandlingMiddleware.cs

---
+++ LibraryAPI/LibraryAPI/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using LibraryAPI.Exceptions;
+
+namespace LibraryAPI.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (NotFoundException e)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(e.Message);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}
