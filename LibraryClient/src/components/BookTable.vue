d42620b9939dcf099c6290dfccf1f1081f943c21
Krystian Wawruch <CresixU@gmail.com>
2023-04-20 09:17:00 +0200

    Created api request with content

--- LibraryClient/src/components/BookTable.vue
+++ LibraryClient/src/components/BookTable.vue
@@ -11,10 +11,38 @@
         </tr>
     </thead>
     <tbody>
+        <tr v-for="book in books" :key="book.title">
+            <td>{{ book.id }}</td>
+            <td>{{ book.title }}</td>
+            <td>{{ book.author }}</td>
+            <td>{{ book.category }}</td>
+            <td>{{ book.publicationYear }}</td>
+            <td>{{ OwnTrueFalseMessage(book.isAvailable) }}</td>
+        </tr>
     </tbody>
 </table>
 </template>
 
 <script>
-    
+export default {
+    data() {
+        return {
+            books: null
+        }
+    },
+    methods: {
+        async fetchData() {
+            const url = `${this.$API_URL}/api/books`
+            const response = await fetch(url)
+            this.books = await response.json()
+        },
+        OwnTrueFalseMessage(val) {
+            if(val) return "Tak"
+            return "Nie"
+        }
+    },
+    created() {
+        this.fetchData()
+    }
+}
 </script>
