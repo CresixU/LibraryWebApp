dce4969e79657258f934ee262092da923c72ede9
Krystian Wawruch <CresixU@gmail.com>
2023-04-29 17:04:05 +0200

    Corrected response and add one more column for table

--- LibraryClient/src/components/BookTable.vue
+++ LibraryClient/src/components/BookTable.vue
@@ -8,6 +8,7 @@
             <th>Kategoria</th>
             <th>Rok publikacji</th>
             <th>Dostępność</th>
+            <th>Actions</th>
         </tr>
     </thead>
     <tbody>
@@ -18,27 +19,36 @@
             <td>{{ book.category }}</td>
             <td>{{ book.publicationYear }}</td>
             <td>{{ OwnTrueFalseMessage(book.isAvailable) }}</td>
+            <td><button v-on:click="Rent(book.id)" data-content="Rent" :disabled="!book.isAvailable" class="btn-main">Rent</button></td>
         </tr>
     </tbody>
 </table>
+<div class="w-100 pagination">
+    Pages...
+</div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            books: null
+            books: null,
+            data: null
         }
     },
     methods: {
-        async fetchData() {
-            const url = `${this.$API_URL}/api/books`
+        async fetchData(page = 1, search = "", category = "") {
+            const url = `${this.$API_URL}/api/books?PageNumber=${page}&PageSize=10`
             const response = await fetch(url)
-            this.books = await response.json()
+            this.data = await response.json()
+            this.books = this.data.items
         },
         OwnTrueFalseMessage(val) {
             if(val) return "Tak"
             return "Nie"
+        },
+        Rent(bookId) {
+            alert("open modal :)")
         }
     },
     created() {
