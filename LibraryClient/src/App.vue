dea011c9e99610ad6868b50d342b8a9f99d790ff
Krystian Wawruch <CresixU@gmail.com>
2023-04-29 17:05:08 +0200

    Getting ready for routes

--- LibraryClient/src/App.vue
+++ LibraryClient/src/App.vue
@@ -1,23 +1,36 @@
 <template>
 <div>
-  <header>
-    Library Application
-  </header>
-  <nav>
-
-  </nav>
-  <aside>
-    <div class="filters">
-      <h3>Filtry</h3>
-      <SearchBox/>
-      <Categories/>
+  <header class="row">
+    <div class="col-6 logo">
+      <i class="bi bi-book-half" style="margin-right: 12px"></i>
+      <span>Library App</span>
+    </div>
+    <div class="col-6">
 
     </div>
-  </aside>
+  </header>
   <main>
-    
-    <div class="data">
-      <BookTable/>
+    <div class="row nav w-100">
+      <div class="col"></div>
+      <MainNavElementItem classProp="bi bi-people">Users</MainNavElementItem>
+      <MainNavElementItem classProp="bi bi-journal">Books</MainNavElementItem>
+      <MainNavElementItem classProp="bi bi-card-list">Rents</MainNavElementItem>
+    </div>
+    <div class="row px-2 py-4">
+      <BooksView></BooksView>
+      <!--
+      <div class="col filters">
+        <h3 class="text-center">Filters</h3>
+        <SearchBox class="my-3"/>
+        <Categories class="my-3"/>
+        <div>
+          <button class="btn-main my-3 d-block px-4" style="margin-left: auto">Search</button>
+        </div>
+      </div>
+      <div class="col">
+        <BookTable/>
+      </div>
+      -->
     </div>
   </main>
   <footer>
@@ -28,15 +41,22 @@
 </template>
 
 <script>
+
+
 import Categories from './components/filters/Categories.vue';
 import SearchBox from './components/filters/SearchBox.vue';
 import BookTable from './components/BookTable.vue';
+import MainNavElementItem from './components/MainNavElementItem.vue';
+
+import BooksView from './views/BooksView.vue'
 
 export default {
   components: {
     Categories,
     SearchBox,
-    BookTable
+    BookTable,
+    MainNavElementItem,
+    BooksView
   }
 }
 </script>
