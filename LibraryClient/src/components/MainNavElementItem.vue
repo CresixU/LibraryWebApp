67dbfee7a0b587d9a8d121ae944bcc9a7dbba1bc
Krystian Wawruch <CresixU@gmail.com>
2023-04-29 17:04:40 +0200

    Create MainNavElementItem.vue

---
+++ LibraryClient/src/components/MainNavElementItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="col nav-element">
+        <i :class="classProp"></i>
+        <span><slot></slot></span>
+      </div>
+</template>
+<script>
+export default {
+    props: {
+        classProp: String
+    }
+}
+</script>
