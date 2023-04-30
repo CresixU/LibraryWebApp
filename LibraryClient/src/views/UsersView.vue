44d2604cd561df07edae92e0b518829068699e0f
Krystian Wawruch <CresixU@gmail.com>
2023-04-30 23:14:22 +0200

    Create UsersView.vue

---
+++ LibraryClient/src/views/UsersView.vue
@@ -0,0 +1,12 @@
+<template>
+<MainUsersTable></MainUsersTable>
+</template>
+
+<script>
+import MainUsersTable from '../components/MainUsersTable.vue'
+export default {
+    components: {
+        MainUsersTable
+    }
+}
+</script>
