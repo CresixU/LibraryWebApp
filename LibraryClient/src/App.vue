76aa64ade2074133b1d96800310c1d9ff4cdf9b8
Krystian Wawruch <CresixU@gmail.com>
2023-04-30 23:16:25 +0200

    Refactored
    Changed elements to router links and removed components that moved to new view

--- LibraryClient/src/App.vue
+++ LibraryClient/src/App.vue
@@ -6,31 +6,23 @@
       <span>Library App</span>
     </div>
     <div class="col-6">
-
     </div>
   </header>
   <main>
     <div class="row nav w-100">
       <div class="col"></div>
-      <MainNavElementItem classProp="bi bi-people">Users</MainNavElementItem>
-      <MainNavElementItem classProp="bi bi-journal">Books</MainNavElementItem>
-      <MainNavElementItem classProp="bi bi-card-list">Rents</MainNavElementItem>
+      <router-link to="/" class="col nav-element">
+        <MainNavElementItem classProp="bi bi-people">Users</MainNavElementItem>
+      </router-link>
+      <router-link to="/books" class="col nav-element">
+        <MainNavElementItem classProp="bi bi-journal">Books</MainNavElementItem>
+      </router-link>
+      <router-link to="/" class="col nav-element">
+        <MainNavElementItem classProp="bi bi-card-list">Rents</MainNavElementItem>
+      </router-link>
     </div>
     <div class="row px-2 py-4">
-      <BooksView></BooksView>
-      <!--
-      <div class="col filters">
-        <h3 class="text-center">Filters</h3>
-        <SearchBox class="my-3"/>
-        <Categories class="my-3"/>
-        <div>
-          <button class="btn-main my-3 d-block px-4" style="margin-left: auto">Search</button>
-        </div>
-      </div>
-      <div class="col">
-        <BookTable/>
-      </div>
-      -->
+      <router-view></router-view>
     </div>
   </main>
   <footer>
@@ -42,21 +34,11 @@
 
 <script>
 
-
-import Categories from './components/filters/Categories.vue';
-import SearchBox from './components/filters/SearchBox.vue';
-import BookTable from './components/BookTable.vue';
 import MainNavElementItem from './components/MainNavElementItem.vue';
-
-import BooksView from './views/BooksView.vue'
 
 export default {
   components: {
-    Categories,
-    SearchBox,
-    BookTable,
     MainNavElementItem,
-    BooksView
   }
 }
 </script>
