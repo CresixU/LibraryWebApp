13dface6979c6e079ef7acfd6f8a520c3ed21ee2
Krystian Wawruch <CresixU@gmail.com>
2023-04-30 23:15:27 +0200

    Add fetch data button

--- LibraryClient/src/components/BookTable.vue
+++ LibraryClient/src/components/BookTable.vue
@@ -18,13 +18,18 @@
             <td>{{ book.author }}</td>
             <td>{{ book.category }}</td>
             <td>{{ book.publicationYear }}</td>
-            <td>{{ OwnTrueFalseMessage(book.isAvailable) }}</td>
+            <td>{{ book.isAvailable ? 'Tak' : 'Nie' }}</td>
             <td><button v-on:click="Rent(book.id)" data-content="Rent" :disabled="!book.isAvailable" class="btn-main">Rent</button></td>
         </tr>
     </tbody>
 </table>
 <div class="w-100 pagination">
-    Pages...
+    <button
+     class="btn-main"
+     style="margin-left:auto"
+      v-on:click="fetchData(page+1,search,category)">
+      Fetch more!
+    </button>
 </div>
 </template>
 
@@ -32,27 +37,30 @@
 export default {
     data() {
         return {
-            books: null,
-            data: null
+            books: [],
+            data: null,
+            page: 1,
+            search: "",
+            category: ""
         }
     },
     methods: {
-        async fetchData(page = 1, search = "", category = "") {
-            const url = `${this.$API_URL}/api/books?PageNumber=${page}&PageSize=10`
+        async fetchData(p, search = "", category = "") {
+            this.page = p
+            this.search = search
+            this.category = category
+            console.log(this.page)
+            const url = `${this.$API_URL}/api/books?PageNumber=${this.page}&PageSize=10`
             const response = await fetch(url)
             this.data = await response.json()
-            this.books = this.data.items
-        },
-        OwnTrueFalseMessage(val) {
-            if(val) return "Tak"
-            return "Nie"
+            this.books.push(...this.data.items)
         },
         Rent(bookId) {
             alert("open modal :)")
-        }
+        },
     },
     created() {
-        this.fetchData()
+        this.fetchData(1)
     }
 }
 </script>
