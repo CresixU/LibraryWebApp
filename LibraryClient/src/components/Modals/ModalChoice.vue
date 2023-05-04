40494f67f357a840942cd5ea3e72c78b0df14683
Krystian Wawruch <CresixU@gmail.com>
2023-05-04 15:09:51 +0200

    Add modal component with choice

---
+++ LibraryClient/src/components/Modals/ModalChoice.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="modal fade" :id="modalId" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">{{ title }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p><slot>Default text</slot></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ btnNo }}</button>
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="CallParentTrueAction()">{{ btnYes }}</button>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    props: {
+        modalId: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String, 
+            default: 'No title'
+        },
+        btnYes: {
+            type: String,
+            default: 'Save changes'
+        },
+        btnNo: {
+            type: String,
+            default: 'Close'
+        },
+        trueData: {}
+    },
+    methods: {
+        CallParentTrueAction() {
+            this.$emit('CallParentTrueAction', this.trueData)
+        }
+    }
+}
+</script>
