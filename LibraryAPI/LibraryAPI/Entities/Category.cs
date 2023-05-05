f2b1a38c9d459c21acd499c12c70ab3ac6eb88b5
Krystian Wawruch <CresixU@gmail.com>
2023-05-05 22:59:31 +0200

    removed list incialization

--- LibraryAPI/LibraryAPI/Entities/Category.cs
+++ LibraryAPI/LibraryAPI/Entities/Category.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public List<Book> Books { get; set; } = new List<Book>();
+        public List<Book> Books { get; set; }
     }
 }
