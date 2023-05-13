98563eb3868ae0087205f3034bab5e014180f9b3
Krystian Wawruch <CresixU@gmail.com>
2023-05-13 12:46:21 +0200

    Create BadRequestException.cs

---
+++ LibraryAPI/LibraryAPI/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAPI.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
