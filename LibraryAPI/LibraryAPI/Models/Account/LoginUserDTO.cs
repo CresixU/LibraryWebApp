1547557fece036832ee1f73228922261af9397c2
Krystian Wawruch <CresixU@gmail.com>
2023-05-13 12:46:48 +0200

    Add dtos to new endpoint

---
+++ LibraryAPI/LibraryAPI/Models/Account/LoginUserDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models.Account
+{
+    public class LoginUserDTO
+    {
+        [Required]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
