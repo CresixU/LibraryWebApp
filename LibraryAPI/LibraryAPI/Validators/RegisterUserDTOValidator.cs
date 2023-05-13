6ee1424f96d72fda8457b5f6f316a30cd5be1c7d
Krystian Wawruch <CresixU@gmail.com>
2023-05-13 12:47:12 +0200

    Add validations for two new DTOs

---
+++ LibraryAPI/LibraryAPI/Validators/RegisterUserDTOValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using LibraryAPI.Models.Account;
+
+namespace LibraryAPI.Validators
+{
+    public class RegisterUserDTOValidator : AbstractValidator<RegisterUserDTO>
+    {
+        public RegisterUserDTOValidator(LibraryContext dbContext)
+        {
+            RuleFor(x => x.Firstname)
+                .NotEmpty();
+
+            RuleFor(x => x.Lastname)
+                .NotEmpty();
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .MinimumLength(6);
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(e => e.Password);
+
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.Any(u => u.Email == value);
+                    if (emailInUse)
+                        context.AddFailure("Email", "That email is taken.");
+                });
+        }
+    }
+}
