6dddd905dbf4205cc8e439c78491ccd02c975b84
Krystian Wawruch <CresixU@gmail.com>
2023-05-14 12:10:17 +0200

    Add vue-cookies

--- LibraryClient/src/main.js
+++ LibraryClient/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
+import VueCookies from 'vue-cookies'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueCookies)
 
 //global variable
 app.config.globalProperties.$API_URL = `https://localhost:7054`
