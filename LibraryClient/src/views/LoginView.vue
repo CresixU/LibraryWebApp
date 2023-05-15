06606e5bd5a6f4d3550b8d03b960d8e889ff8792
Krystian Wawruch <CresixU@gmail.com>
2023-05-15 16:44:02 +0200

    Create LoginView.vue

---
+++ LibraryClient/src/views/LoginView.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="box">
+        <div class="header">
+            <h2>Login Panel</h2>
+        </div>
+        <div class="login-box">
+            <div>
+                <label for="login">Email</label>
+                <input type="text" id="login" placeholder="Your email" v-model="email">
+            </div>
+            <div>
+                <label for="pass">Password</label>
+                <input type="password" id="pass" placeholder="Your password" v-model="password">
+            </div>
+            <div>
+                <button class="btn-main mt-4 w-100" @click="LogIn()">Log in</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import router from '../router'
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async LogIn() {
+            const url = `${this.$API_URL}/api/account/login`
+            const response = await fetch(url, {
+                method: 'POST',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({"email": this.email, "password": this.password})
+            })
+            .then(response => response.text())
+            .then(data => {
+                this.$cookies.set('auth',data)
+                this.$router.replace('/panel')
+            })
+        },
+        CheckCookies() {
+            if(this.$cookies.isKey('auth')) router.replace('/panel')
+        }
+    },
+    created() {
+        this.CheckCookies()
+    }
+}
+</script>
+<style scoped>
+.box {
+    position: absolute;
+    border-radius: 15px 15px 0px 0px;
+    width: 700px;
+    background-color: rgb(250, 211, 255);
+    left: 50%;
+    right: 50%;
+    transform: translate(-50%);
+}
+.header {
+    background: var(--main-gradient);
+    border-radius: 15px 15px 0px 0px;
+    padding: 5px 10px;
+}
+.header h2 {
+    color: white;
+    text-align: center;
+}
+.login-box {
+    padding: 30px;
+    max-width: 500px;
+    margin: auto;
+}
+.login-box  input {
+    border-bottom: 1px solid blue;
+    margin-bottom: 10px;
+}
+</style>
