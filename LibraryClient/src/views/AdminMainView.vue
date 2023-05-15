36c9ddde5117f97e1f6fa4a9b4133505be3e0e1c
Krystian Wawruch <CresixU@gmail.com>
2023-05-15 16:48:05 +0200

    Moved code from App.vue (nav bar)

--- LibraryClient/src/views/AdminMainView.vue
+++ LibraryClient/src/views/AdminMainView.vue
@@ -1,3 +1,32 @@
 <template>
-    <h2 class="text-center">Hello</h2>
+    <div class="row nav w-100">
+        <div class="col-3"></div>
+        <div class="col-9 row">
+          <router-link to="/panel/users" class="col nav-element">
+            <MainNavElementItem classProp="bi bi-people">Users</MainNavElementItem>
+          </router-link>
+          <router-link to="/panel/books" class="col nav-element">
+            <MainNavElementItem classProp="bi bi-journal">Books</MainNavElementItem>
+          </router-link>
+          <router-link to="/panel/rents" class="col nav-element">
+            <MainNavElementItem classProp="bi bi-card-list">Rents</MainNavElementItem>
+          </router-link>
+          <router-link to="/admin" class="col nav-element">
+            <MainNavElementItem classProp="bi bi-person-gear">Panel</MainNavElementItem>
+          </router-link>
+        </div>
+      </div>
+      <div class="row px-2 py-4">
+        <router-view></router-view>
+      </div>
 </template>
+<script>
+
+import MainNavElementItem from '../components/MainNavElementItem.vue';
+
+export default {
+  components: {
+    MainNavElementItem,
+  }
+}
+</script>
