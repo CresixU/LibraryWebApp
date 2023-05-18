3794f146ebfdb20731444d6fa85df8ae10a7150b
Krystian Wawruch <CresixU@gmail.com>
2023-05-18 09:36:08 +0200

    remove required due to fluent validation

--- LibraryAPI/LibraryAPI/Models/Account/LoginUserDTO.cs
+++ LibraryAPI/LibraryAPI/Models/Account/LoginUserDTO.cs
@@ -4,7 +4,6 @@ namespace LibraryAPI.Models.Account
 {
     public class LoginUserDTO
     {
-        [Required]
         public string Email { get; set; }
         public string Password { get; set; }
     }
