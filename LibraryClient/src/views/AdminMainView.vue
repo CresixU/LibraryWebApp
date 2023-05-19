191bd8780b21e78903949fcf11772472fe56dc60
Krystian Wawruch <CresixU@gmail.com>
2023-05-19 11:40:48 +0200

    Add color to display active tab

--- LibraryClient/src/views/AdminMainView.vue
+++ LibraryClient/src/views/AdminMainView.vue
@@ -2,16 +2,16 @@
     <div class="row nav w-100">
         <div class="col-3"></div>
         <div class="col-9 row">
-          <router-link to="/panel/users" class="col nav-element">
+          <router-link exact-active-class="active" to="/panel/users" class="col nav-element">
             <MainNavElementItem classProp="bi bi-people">Users</MainNavElementItem>
           </router-link>
-          <router-link to="/panel/books" class="col nav-element">
+          <router-link exact-active-class="active" to="/panel/books" class="col nav-element">
             <MainNavElementItem classProp="bi bi-journal">Books</MainNavElementItem>
           </router-link>
-          <router-link to="/panel/rents" class="col nav-element">
+          <router-link exact-active-class="active" to="/panel/rents" class="col nav-element">
             <MainNavElementItem classProp="bi bi-card-list">Rents</MainNavElementItem>
           </router-link>
-          <router-link to="/admin" class="col nav-element">
+          <router-link exact-active-class="active" to="/admin" class="col nav-element">
             <MainNavElementItem classProp="bi bi-person-gear">Panel</MainNavElementItem>
           </router-link>
         </div>
@@ -30,3 +30,8 @@ export default {
   }
 }
 </script>
+<style scroped>
+.active > *{
+  color: #10d7e1
+}
+</style>
