453869e02cc887ced341f6dd3da53f1523f5039f
Krystian Wawruch <CresixU@gmail.com>
2023-06-11 15:09:06 +0200

    Add role validation functions

--- LibraryClient/src/store.js
+++ LibraryClient/src/store.js
@@ -2,5 +2,19 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-  isUserLogged: false
+  isUserLogged: false,
+  user: null,
+  IsAdmin() {
+    if(this.user.RoleName == 'Admin') return true
+    return false
+  },
+  IsOwner() {
+    if(this.user.RoleName == 'Admin' || this.user.RoleName == 'Owner')
+      return true
+    return false
+  },
+  IsUser() {
+    if(this.user.RoleName == 'User') return true
+    return false
+  }
 })
