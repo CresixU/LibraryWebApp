0e19ae3554a1230a3382d357f901be77aa203223
Krystian Wawruch <CresixU@gmail.com>
2023-08-05 14:44:38 +0200

    Added Index for Users and Books

--- LibraryAPI/LibraryAPI/Migrations/20230805124330_AddIndexForUsersAndBooks.Designer.cs
+++ LibraryAPI/LibraryAPI/Migrations/20230805124330_AddIndexForUsersAndBooks.Designer.cs
@@ -4,6 +4,7 @@ using LibraryAPI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryAPI.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20230805124330_AddIndexForUsersAndBooks")]
+    partial class AddIndexForUsersAndBooks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
